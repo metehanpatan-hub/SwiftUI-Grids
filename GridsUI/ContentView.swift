5fff52e6ad08bea80e4533e01b1cfa74c15d895a
metehanpatan-hub <metehan.patan@gmail.com>
2025-03-06 15:35:08 +0300

    Vertical Grid View Summary

--- GridsUI/ContentView.swift
+++ GridsUI/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns = Array(repeating:GridItem(.flexible(), spacing:10), count:4)
+    
     var body: some View {
-        LazyVGrid(columns:[GridItem(.flexible())]){
-            ForEach(MockData.colors, id: \.self) {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill($0.gradient)
-                    .frame(height: 100) 
+        ScrollView{
+            LazyVGrid(columns: columns, pinnedViews: .sectionHeaders){
+                Section{
+                    ForEach(MockData.colors, id: \.self) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill($0.gradient)
+                            .frame(height: 100)
+                    }
+                } header: {
+                    Text("Favorites")
+                        .font(.largeTitle.bold())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                Section{
+                    ForEach(MockData.colors, id: \.self) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill($0.gradient)
+                            .frame(height: 100)
+                    }
+                } header: {
+                    Text("Not Favorites")
+                        .font(.largeTitle.bold())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
             }
+            .padding()
         }
     }
 }
-
 #Preview {
     ContentView()
 }
